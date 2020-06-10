94741f4b0bf265c41177618048ec5a187a5cd277
Felix-Redwood <Felix@grahamredwood.com>
2020-06-10 14:56:14 +0000

    Added 'register' view

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from accounts.forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
 # Views for the 'accounts' app.
 
 
-def logout(request):
-    """Logs the user out of their account"""
-    auth.logout(request)
-    messages.success(request, "You have succesfully logged out")
-    return redirect(reverse('index'))
-
-
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -30,3 +26,38 @@ def login(request):
     else:
         login_form = UserLoginForm()
     return render(request, "login.html", {'login_form': login_form})
+
+
+def register(request):
+    """Render the register.html page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have succesfully registered")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request,
+                               "Unable to register your account at this time")
+
+    else:
+        registration_form = UserRegistrationForm()
+
+    return render(request, "register.html",
+                  {"registration_form": registration_form})
+
+
+@login_required
+def logout(request):
+    """Logs the user out of their account"""
+    auth.logout(request)
+    messages.success(request, "You have succesfully logged out")
+    return redirect(reverse('index'))
