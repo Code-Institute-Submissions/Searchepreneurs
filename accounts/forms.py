4ede3aa42687da28d1aa5f9f94a3af1b091267ff
Felix-Redwood <Felix@grahamredwood.com>
2020-06-10 14:55:59 +0000

    Created UserRegistrationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 # Forms for the 'accounts' app
@@ -9,3 +12,42 @@ class UserLoginForm(forms.Form):
 
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(UserCreationForm):
+    """Form used to register a new user."""
+
+    password1 = forms.CharField(label="Password",
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password",
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(u'Email Address must be unique')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(username=username).exclude(email=email):
+            raise forms.ValidationError(u'Username must be unique')
+        return username
+
+    def clean_password(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1 or password2:
+            raise ValidationError('Please confirm your password')
+
+        if password1 != password2:
+            raise ValidationError('Passwords do not match')
+
+        return password2
