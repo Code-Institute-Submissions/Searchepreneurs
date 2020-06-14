f7ed492b836b4986131f21a223c28dc1b8016bd0
Felix-Redwood <Felix@grahamredwood.com>
2020-06-14 13:04:34 +0000

    Created 'Purchase' and 'Client' models

---
+++ checkout/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from services.models import Service
+from django.contrib.auth.models import User
+
+# Models for the 'checkout' app
+
+
+class Purchase(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=50, blank=True)
+    purchase_email = models.EmailField(max_length=50)
+    username = models.CharField(max_length=150, null=False)
+    country = models.CharField(max_length=40, blank=False)
+    region = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}-{3}".format(self.id,
+                                        self.date,
+                                        self.full_name,
+                                        self.username)
+
+
+class Client(models.Model):
+    purchase = models.ForeignKey(
+        Purchase,
+        null=False,
+        on_delete=models.CASCADE
+    )
+    service = models.ForeignKey(
+        Service,
+        null=False,
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        null=False,
+        on_delete=models.CASCADE
+    )
+    client_email = models.EmailField(max_length=50)
+    client_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return "{0}-{1}-{2} @ {3}".format(self.service.name,
+                                          self.user.username,
+                                          self.client_url,
+                                          self.purchase.date)
