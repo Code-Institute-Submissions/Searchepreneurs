4ea3a033dda637fe8d2447ba1a3b74cdd30bfef7
Felix-Redwood <Felix@grahamredwood.com>
2020-06-25 13:15:34 +0000

    Created view to render privacy_policy.html

--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,8 @@ from django.shortcuts import render
 def index(request):
     """Renders the index.html page"""
     return render(request, "index.html")
+
+
+def privacy_policy(request):
+    """Renders the privacy_policy.html page"""
+    return render(request, "privacy_policy.html")
