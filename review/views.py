da1db440051b93b16e140b286c6ec962cb192236
Felix-Redwood <Felix@grahamredwood.com>
2020-07-02 12:47:45 +0000

    Changed client's review_done field to True upon creation of a review

--- review/views.py
+++ review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .forms import ReviewForm
 from .models import Review
 from django.contrib.auth.models import User
@@ -19,8 +19,10 @@ def write_review(request, id):
     """Render a form where a user can submit a review."""
     if request.method == "POST":
         review_form = ReviewForm(request.POST)
+        print(review_form)
         user = User.objects.get(email=request.user.email)
         client = Client.objects.get(id=id)
+        print(client)
 
         if review_form.is_valid():
             print("REVIEW FORM IS VALID")
@@ -28,9 +30,17 @@ def write_review(request, id):
             review.review_username = user.username
             review.review_service = client.service.name
             review.save()
+            client.review_done = True
+            client.save()
             return redirect('reviews')
+
+        else:
+            print("INVALID REVIEW FORM")
+            print(review_form.errors)
     else:
         review_form = ReviewForm()
+        client = Client.objects.get(id=id)
         return render(request,
                       "review_form.html",
-                      {"review_form": review_form})
+                      {"review_form": review_form,
+                       "client": client})
