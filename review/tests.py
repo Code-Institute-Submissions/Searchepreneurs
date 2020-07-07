fa333857babdfe088b24d2bc5127c02295895f24
Felix-Redwood <Felix@grahamredwood.com>
2020-07-07 13:11:05 +0000

    Added ReviewForm tests

--- review/tests.py
+++ review/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .forms import ReviewForm
 
-# Create your tests here.
+
+# Tests for the 'review' app
+
+
+class TestReview(TestCase):
+
+    def test_can_review_without_title(self):
+        form = ReviewForm({'rating': 10,
+                           'review_description': 'A review'})
+        self.assertFalse(form.is_valid(), form.errors)
+
+    def test_full_review_form_is_valid(self):
+        form = ReviewForm({'rating': 10,
+                           'review_title': 'Review Title',
+                           'review_description': 'A review'})
+        self.assertTrue(form.is_valid(), form.errors)
